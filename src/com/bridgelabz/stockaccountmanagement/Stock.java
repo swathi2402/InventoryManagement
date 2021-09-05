287f049fc42c3387b4821a7463779894bfc57418
Swathi <swathi.h@perfios.com>
2021-09-05 16:55:35 +0530

    ADDED: Class to store stock details

---
+++ src/com/bridgelabz/stockaccountmanagement/Stock.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.stockaccountmanagement;
+
+public class Stock {
+	private String name;
+	private int numberOfShares;
+	private double sharePrice;
+
+	public Stock(String name, int numberOfShares, double sharePrice) {
+		super();
+		this.name = name;
+		this.numberOfShares = numberOfShares;
+		this.sharePrice = sharePrice;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumberOfShares() {
+		return numberOfShares;
+	}
+
+	public void setNumberOfShares(int numberOfShares) {
+		this.numberOfShares = numberOfShares;
+	}
+
+	public double getSharePrice() {
+		return sharePrice;
+	}
+
+	public void setSharePrice(double sharePrice) {
+		this.sharePrice = sharePrice;
+	}
+}
