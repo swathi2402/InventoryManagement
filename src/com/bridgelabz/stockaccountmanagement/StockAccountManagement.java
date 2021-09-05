db2256c3b479a1303ebba4430c303ba527a14dc1
Swathi <swathi.h@perfios.com>
2021-09-05 16:10:24 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/stockaccountmanagement/StockAccountManagement.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.stockaccountmanagement;
+
+public class StockAccountManagement {
+
+	public static void main(String[] args) {
+		System.out.println("*** Welcome to Stock Account Management ***");
+	}
+
+}
