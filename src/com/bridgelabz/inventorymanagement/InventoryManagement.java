6e814c30392b83336eb68806254624165891943d
Swathi <swathi.h@perfios.com>
2021-09-05 14:51:18 +0530

    Initial Commit

---
+++ src/com/bridgelabz/inventorymanagement/InventoryManagement.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.inventorymanagement;
+
+public class InventoryManagement {
+
+	public static void main(String[] args) {
+		System.out.println("*** Welcome to Inventory Data Management ***");
+	}
+
+}
